8b12c3102b00c129a80ed6806da23c12e0a21e15
Fani Aghistiya Lestari <fani.aghistiya.lestr@gmail.com>
2022-12-13 21:58:45 +0700

    Slicing Layout 1

--- app/src/main/java/binar/academy/flightgoadmin/view/DetailTiketFragment.kt
+++ app/src/main/java/binar/academy/flightgoadmin/view/DetailTiketFragment.kt
@@ -7,16 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import binar.academy.flightgoadmin.R
 
-class HomeFragment : Fragment() {
+
+class DetailTiketFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_detail_tiket, container, false)
     }
-
 
 }
