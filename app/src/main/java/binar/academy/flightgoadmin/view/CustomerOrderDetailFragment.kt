f93124f95e4acc755bc524dae3698fc50335da1e
ibal.libul <ibal.libul>
2022-12-15 14:29:14 +0700

    Slicing Layout finish

---
+++ app/src/main/java/binar/academy/flightgoadmin/view/CustomerOrderDetailFragment.kt
@@ -0,0 +1,23 @@
+package binar.academy.flightgoadmin.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import binar.academy.flightgoadmin.R
+
+
+class CustomerOrderDetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer_order_detail, container, false)
+    }
+
+
+}
